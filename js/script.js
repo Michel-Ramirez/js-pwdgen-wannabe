b52b874d97aa9956d10fa3759537e3865189d25b
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-25 16:14:49 +0200

    Finish js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@ console.log('JS OK')
 
 const pswPlaceholder = document.getElementById('psw');
 console.log(pswPlaceholder);
+
+const fnamePlaceholder = document.getElementById('fname')
+
+const lnamePlaceholder = document.getElementById('lname')
+
+const colorPlaceholder = document.getElementById('pref-color')
+
+//Dichiaro la variabile assoluta che verrà appostat alla fine della password uguale per tutti
+const defaultNumber = 21
+
+//Interrogo nome utente
+const fName = prompt('Come ti chiami?');
+console.log(fName)
+
+
+//Interrogo Cognome utente
+const lName = prompt('Qual è il tuo cognome?');
+console.log(lName)
+
+//Interrogo qual è il suo colore preferito
+const userColor = prompt('Qual è il tuo colore prederito?');
+console.log(userColor)
+
+//Scrivo sul form i dati
+
+fnamePlaceholder.innerText += ' ' + fName
+
+lnamePlaceholder.innerText += ' ' + lName
+
+colorPlaceholder.innerText += ' ' + userColor
+
+//Genero la password
+
+pswPlaceholder.innerText += ' ' + fName + lName + userColor + defaultNumber
